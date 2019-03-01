b1df1bcf2840036a155155c5afd4973dc47ce653
kanekotaiki <t-smile-evd@i.softbank.jp>
2019-03-01 14:36:23 +0000

    toppage_login.user_show.new.edit.sessions

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -14,29 +14,6 @@ $(function() {
 
 
 $(document).ready(function() {
-  // animatedクラスの付いた要素にwaypointを登録
-  $(".sannpuru").waypoint({
-  	handler: function(direction) {
-      // 要素が画面中央に来るとここが実行される
-      if (direction === "down") { // 下方向のスクロール
-        // イベント発生元の要素にfadeInUpクラスを付けることで
-        // アニメーションを開始
-        $(".title").addClass("zoomIn");
-        $(".setumei").addClass("zoomIn");
-        $(".setumei2").addClass("zoomIn");
-        $(".setumei3").addClass("zoomIn");
-        $(".setumei4").addClass("zoomIn");
-        // waypointを削除することで、この要素に対しては
-        // これ以降handlerが呼ばれなくなる
-        
-      }
-  	},
-    offset: "70%"
-});
-});
-
-
-$(document).ready(function() {
   // popupクラスを持つ要素にMagnific Popupを適用
   $(".popup").magnificPopup({
     type: "image",
